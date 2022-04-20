96a4780dd993f67897f84a33d52390f917c5e2e7
raffli <rafflitok@gmail.com>
2022-04-20 21:26:19 +0700

    add layout for recycler view rekap tadarus

---
+++ app/src/main/java/com/raffli/tadarusmobile/RekapTadarusActivity.kt
@@ -0,0 +1,11 @@
+package com.raffli.tadarusmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RekapTadarusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rekap_tadarus)
+    }
+}
