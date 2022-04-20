83915d87fd8d3b2dbd7203749684fe89deaa5353
raffli <rafflitok@gmail.com>
2022-04-20 20:41:45 +0700

    initial project and create menu and tambah catatan layout

---
+++ app/src/main/java/com/raffli/tadarusmobile/TambahCatatanActivity.kt
@@ -0,0 +1,11 @@
+package com.raffli.tadarusmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahCatatanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_catatan)
+    }
+}
